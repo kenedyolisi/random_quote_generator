8a248654fe4bc1b4bec3e2de28bd6dee52413815
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-01-24 01:07:46 +0300

    fix: update import paths after rename

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { renderToString } from "react-dom/server";
-import App from "./App";
+import App from "./app";
 
 export function render() {
   const html = renderToString(
