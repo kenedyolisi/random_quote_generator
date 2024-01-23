6b31bf5043bad41d04a1a82672597acdfb2c1e31
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-01-24 00:52:22 +0300

    refactor: update entry-client.tsx

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,11 +1,13 @@
-import './index.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import "./index.css";
+import { StrictMode } from "react";
+import { hydrateRoot } from "react-dom/client";
+import App from "./App";
 
-ReactDOM.hydrateRoot(
-  document.getElementById('root') as HTMLElement,
-  <React.StrictMode>
+const rootElem = document.getElementById("root")!;
+
+hydrateRoot(
+  rootElem,
+  <StrictMode>
     <App />
-  </React.StrictMode>
-)
+  </StrictMode>
+);
