c57a107f1c37274412bf30566c70d450831f0048
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-03-19 17:49:11 +0300

    feat: add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import ViteYaml from "@modyfi/vite-plugin-yaml";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/random-quote-generator/",
-  plugins: [svelte(), ViteYaml()],
+  plugins: [
+    svelte(),
+    ViteYaml(),
+    VitePWA({ registerType: "autoUpdate", devOptions: { enabled: true } }),
+  ],
 });
