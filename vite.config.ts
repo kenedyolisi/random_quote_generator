5e9b4f009418476f68735be701cabf778e7ef15f
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-11-26 20:38:34 +0300

    chore: add tailwindcss vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import ViteYaml from "@modyfi/vite-plugin-yaml";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
@@ -8,6 +9,7 @@ export default defineConfig({
   base: "/random-quote-generator/",
   plugins: [
     svelte(),
+    tailwindcss(),
     ViteYaml(),
     VitePWA({ registerType: "autoUpdate", devOptions: { enabled: true } }),
   ],
