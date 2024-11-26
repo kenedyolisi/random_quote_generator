730181c27172117e0dc9f79a161c76ca8e0ecbb1
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-11-26 22:06:28 +0300

    chore: delete tailwind.config.ts

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,svelte}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-} satisfies Config;
