a7ec1f3fabe285bf46620418a7a4521fbb94f73c
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-11-27 12:46:06 +0300

    chore: update base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/random-quote-generator/",
+  base: "/random_quote_generator/",
   plugins: [
     svelte(),
     tailwindcss(),
